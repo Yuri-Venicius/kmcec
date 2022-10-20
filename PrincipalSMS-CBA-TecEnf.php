26ca493b6301a327fc23e0fc7ca4db509dbfe3e4
Yuri Oliveira <yvenicios@gmail.com>
2022-10-20 12:53:15 -0300

    Add Aula 18-10 SMS TODOS GOUVEIA

--- PrincipalSMS-CBA-TecEnf.php
+++ PrincipalSMS-CBA-TecEnf.php
@@ -216,6 +216,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="collapse" id="collapsePortuguesGouveia">
                                 <div class="card card-body">
                                     <a href="SMS_TODOS_PORTUGUES_GOUVEIA_AULA01.php">AULA 01 - GRAVAÇÃO 06/10/2022</a><p></p> 
+                                    <a href="SMS_TODOS_PORTUGUES_GOUVEIA_AULA02.php">AULA 02 - GRAVAÇÃO 18/10/2022</a><p></p> 
                                 </div>
                             </div>
                         </div>
--- SMS_TODOS_PORTUGUES_GOUVEIA_AULA02.php
+++ SMS_TODOS_PORTUGUES_GOUVEIA_AULA02.php
@@ -91,11 +91,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>CURSO PARA A SMS CUIABÁ - PORTUGUÊS | PROFESSOR GOUVEIA</h2><h3>AULA 01</h3>
+                <h2>CURSO PARA A SMS CUIABÁ - PORTUGUÊS | PROFESSOR GOUVEIA</h2><h3>AULA 02</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SMS_TODOS_PORTUGUES_GOUVEIA_AULA02.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SMS_TODOS_PORTUGUES_GOUVEIA_AULA01.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="SMS_TODOS_PORTUGUES_GOUVEIA_AULA03.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/gmodwz" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/5ih55h" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 <div class="col-sm-6" style="margin: center">
