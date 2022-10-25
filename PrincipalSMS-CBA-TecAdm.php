bad505af3d67aefa490f2bb6a089528099a84273
Yuri Oliveira <yvenicios@gmail.com>
2022-10-25 13:31:52 -0300

    Alterado link da aula 1 de geografia SMS TODOS

--- PrincipalSMS-CBA-TecAdm.php
+++ PrincipalSMS-CBA-TecAdm.php
@@ -293,7 +293,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             </p>
                             <div class="collapse" id="collapseGeografiaMarcelo">
                                 <div class="card card-body">
-                                    <!-- <a href="SMS_TODOS_GEOGRAFIA_MARCELO_AULA01.php">AULA 01 - GRAVAÇÃO 13/10/2022</a><p></p>  -->
+                                    <a href="SMS_TODOS_GEOGRAFIA_MARCELO_AULA01.php">AULA 01 - GRAVAÇÃO 13/10/2022</a><p></p> 
                                 </div>
                             </div>
                         </div>
