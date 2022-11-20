3070c80e33dd17276d32afff32a824805d3a41ff
Yuri Oliveira <yvenicios@gmail.com>
2022-11-20 11:21:43 -0300

    Add aula 17-11 INSS SILVIO BONONI

--- INSS_INFORMATICA_SILVIO_AULA02.php
+++ INSS_INFORMATICA_SILVIO_AULA02.php
@@ -96,11 +96,11 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
             ) : ?>
             
 
-                <h2>INSS - NOÇÕES DE INFORMÁTICA | PROFESSOR SILVIO BONONI</h2><h3>AULA 01</h3>
+                <h2>INSS - NOÇÕES DE INFORMÁTICA | PROFESSOR SILVIO BONONI</h2><h3>AULA 02</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="INSS_INFORMATICA_SILVIO_AULA02.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="INSS_INFORMATICA_SILVIO_AULA01.php">Anterior</a>
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/t8xbkv" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/r9o8j7" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 <div class="col-sm-6" style="margin: center">
--- PrincipalINSS.php
+++ PrincipalINSS.php
@@ -318,6 +318,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseInformaticaSilvio">
                                     <div class="card card-body">
                                         <a href="INSS_INFORMATICA_SILVIO_AULA01.php">AULA 01 - GRAVAÇÃO 16/11/2022</a><p></p>
+                                        <a href="INSS_INFORMATICA_SILVIO_AULA02.php">AULA 02 - GRAVAÇÃO 17/11/2022</a><p></p>
                                     </div>
                                 </div>
                             </div>  
