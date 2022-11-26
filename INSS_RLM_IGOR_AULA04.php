542d42d5320b22a1be302b0278ba532889c54af6
Yuri Oliveira <yvenicios@gmail.com>
2022-11-26 12:53:55 -0300

    Add aula Final INSS

--- INSS_RLM_IGOR_AULA04.php
+++ INSS_RLM_IGOR_AULA04.php
@@ -96,12 +96,11 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
             ) : ?>
             
 
-                <h2>INSS - RACIOCÍNIO LÓGICO | PROFESSOR IGOR BRASIL</h2><h3>AULA 03</h3>
+                <h2>INSS - RACIOCÍNIO LÓGICO | PROFESSOR IGOR BRASIL</h2><h3>AULA 04</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="INSS_RLM_IGOR_AULA02.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="INSS_RLM_IGOR_AULA04.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="INSS_RLM_IGOR_AULA03.php">Anterior</a>
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/n4md6a" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/28nrl2" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 <div class="col-sm-6" style="margin: center">
--- PrincipalINSS.php
+++ PrincipalINSS.php
@@ -277,6 +277,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="INSS_RLM_IGOR_AULA01.php">AULA 01 - GRAVAÇÃO 07/10/2022</a><p></p>
                                         <a href="INSS_RLM_IGOR_AULA02.php">AULA 02 - GRAVAÇÃO 21/10/2022</a><p></p>
                                         <a href="INSS_RLM_IGOR_AULA03.php">AULA 03 - GRAVAÇÃO 25/10/2022</a><p></p>
+                                        <a href="INSS_RLM_IGOR_AULA04.php">AULA 04 - GRAVAÇÃO 25/11/2022</a><p></p>
                                     </div>
                                 </div>
                             </div>
