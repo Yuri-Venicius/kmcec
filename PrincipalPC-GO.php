82ebe5f27424e9bd773443f94e5a931b62819ed0
Yuri Oliveira <yvenicios@gmail.com>
2022-12-03 15:40:56 -0300

    Add aula 30-11 e 01-12 -- PC-GO

--- PrincipalPC-GO.php
+++ PrincipalPC-GO.php
@@ -232,6 +232,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="PC-GO_DIR_CONST_NETO_AULA02.php">AULA 02 - GRAVAÇÃO 05/10/2022</a>
                                         <a href="PC-GO_DIR_CONST_NETO_AULA03.php">AULA 03 - GRAVAÇÃO 05/10/2022</a>
                                         <a href="PC-GO_DIR_CONST_NETO_AULA04.php">AULA 04 - GRAVAÇÃO 31/10/2022</a>
+                                        <a href="PC-GO_DIR_CONST_NETO_AULA05.php">AULA 05 - GRAVAÇÃO 30/11/2022</a>
                                         <p></p>
                                     </div>
                                 </div>
@@ -329,6 +330,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="PC-GO_PROCESSO_PENAL_MARCIANO_AULA01.php">AULA 01 - GRAVAÇÃO 04/10/2022</a>
                                         <a href="PC-GO_PROCESSO_PENAL_MARCIANO_AULA02.php">AULA 02 - GRAVAÇÃO 13/10/2022</a>
                                         <a href="PC-GO_PROCESSO_PENAL_MARCIANO_AULA03.php">AULA 03 - GRAVAÇÃO 03/11/2022</a>
+                                        <a href="PC-GO_PROCESSO_PENAL_MARCIANO_AULA04.php">AULA 04 - GRAVAÇÃO 01/12/2022</a>
                                         <p></p>
                                     </div>
                                 </div>
