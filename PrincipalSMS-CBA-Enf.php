dab6b095bc30d873a32e56c61133325b2af317b1
Yuri Oliveira <yvenicios@gmail.com>
2023-01-20 14:12:54 -0300

    Add aula 19-01 Leg Sus Davoli -- SMS TODOS

--- PrincipalSMS-CBA-Enf.php
+++ PrincipalSMS-CBA-Enf.php
@@ -396,7 +396,8 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA02.php">AULA 02 - GRAVAÇÃO 11/11/2022</a><p></p> 
                                     <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA03.php">AULA 03 - GRAVAÇÃO 17/11/2022</a><p></p> 
                                     <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA04.php">AULA 04 - GRAVAÇÃO 08/12/2022</a><p></p> 
-                                    <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA05.php">AULA 05 - GRAVAÇÃO 20/12/2022</a><p></p> 
+                                    <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA05.php">AULA 05 - GRAVAÇÃO 20/12/2022</a><p></p>
+                                    <a href="SMS_TODOS_LEGISLACAO_DAVOLI_AULA06.php">AULA 06 - GRAVAÇÃO 19/01/2023</a><p></p> 
                                 </div>
                             </div>
                         </div>
