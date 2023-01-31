203c0aa494cca0c9701a8ea4f2142ef4602a8485
Yuri Oliveira <yvenicios@gmail.com>
2023-01-30 21:40:06 -0300

    Add aula 27-01 Portugues -- BB

--- PrincipalBancoDoBrasil.php
+++ PrincipalBancoDoBrasil.php
@@ -177,8 +177,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapsePortuguesGouveia">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
-
+                                        <a href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA01.php">AULA 01 - GRAVAÇÃO 27/01/2023</a>
                                     </div>
                                 </div>
                             </div>
