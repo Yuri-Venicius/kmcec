a0d3f5ac7f1b5e21db626be3e24c4ba62d1d9b45
Yuri Oliveira <yvenicios@gmail.com>
2023-02-15 18:34:08 -0300

    Hotfix -- CadastroUsuarioCurso

--- cadastroUsuarioCurso.php
+++ cadastroUsuarioCurso.php
@@ -9,7 +9,7 @@ $cpfComoSenha = MD5($_POST['cpfParaCurso']);
 $codCurso = $_POST['codCurso'];
 $modalidadeAluno = $_POST['modalidadeAluno'];
 
-$query_select = "SELECT id, usuario, nome FROM usuarios WHERE cpf = '$cpfParaCurso' or senha = '$cpfComoSenha'";
+$query_select = "SELECT * FROM usuarios WHERE cpf = '$cpfParaCurso' or senha = '$cpfComoSenha'";
 $select = mysqli_query($conexao, $query_select);
 $array = mysqli_fetch_array($select);
 $logarray = $array['usuario'];
@@ -27,54 +27,44 @@ $emailUsuario = $array['email'];
             window.location.href='novoUsuario.php';</script>";
 
     }else{
-        if($logarray == $usuario){
-            
-            echo"<script language='javascript' type='text/javascript'>
-            alert('Usuário $nomeUsuario ja está cadastrado, consulte sua situação!');
-            window.location.href='novoUsuario.php';</script>";
-            die();
         
-        }else{
-            $query = "INSERT INTO acessousuariocurso(
-                        idUsuario,
-                        idCurso,
-                        dtCriacaoAcesso,
-                        statusAcesso,
-                        criadoPor,
-                        modalidadeAluno
-                    )
-                    SELECT
-                        '$idUsuario',
-                        '$codCurso',
-                        NOW(), 
-                        'ATIVO', 
-                        '{$_SESSION['usuario']}', 
-                        '$modalidadeAluno'
-                    FROM
-                        acessousuariocurso
-                    WHERE NOT
-                        EXISTS(
-                        SELECT
-                            idUsuario,
-                            idCurso
-                        FROM
-                            acessousuariocurso
-                        WHERE
-                            idUsuario = '$idUsuario' AND curso idCurso = '$codCurso'
-                    )
-                    LIMIT 1";
-            $insert = mysqli_query($conexao, $query);
 
-            if($insert){
-                ($nomeUsuario == null) ? $var = $logarray : $var = $nomeUsuario;
-                ($array['email'] == null) ? enviaEmailPeloServer($conexao, $logarray, $codCurso) : enviaEmailPeloServer($conexao, $array['usuario'], $codCurso);
+            $query_select1 = "SELECT * from acessousuariocurso WHERE idUsuario = '$idUsuario' and idCurso = '$codCurso'";
+            $resultado = mysqli_query($conexao, $query_select1);
+            $countRow = mysqli_num_rows($resultado);
+
+            if($countRow < 1){
+                $queryInsert = "INSERT INTO acessousuariocurso(
+                            idUsuario,
+                            idCurso,
+                            dtCriacaoAcesso,
+                            statusAcesso,
+                            criadoPor,
+                            modalidadeAluno
+                        )
+                        VALUES(
+                            '$idUsuario',
+                            '$codCurso',
+                            NOW(), 
+                            'ATIVO', 
+                            '{$_SESSION['usuario']}', 
+                            '$modalidadeAluno')";
+
+                $insert = mysqli_query($conexao, $queryInsert);
+                
+                if($insert){
+                    ($nomeUsuario == null) ? $var = $logarray : $var = $nomeUsuario;
+                    ($array['email'] == null) ? enviaEmailPeloServer($conexao, $logarray, $codCurso) : enviaEmailPeloServer($conexao, $array['usuario'], $codCurso);
+                    echo"<script language='javascript' type='text/javascript'>
+                        alert('Usuário $var cadastrado em: {$nomeCurso['nomeCurso']} com sucesso!');
+                        window.location.href='novoUsuario.php';</script>";
+                }
+            }
+            
+            if ($countRow >= 1){
+                ($nomeUsuario == null) ? $var1 = $logarray : $var1 = $nomeUsuario;
                 echo"<script language='javascript' type='text/javascript'>
-                    alert('Usuário $var cadastrado em: {$nomeCurso['nomeCurso']} com sucesso!');
-                    window.location.href='novoUsuario.php';</script>";
-            }else{
-                echo"<script language='javascript' type='text/javascript'>
-                    alert('O Usuário $var ja esta cadastrado no curso: {$nomeCurso['nomeCurso']} ');
+                    alert('O Usuário $var1 ja esta cadastrado no curso: {$nomeCurso['nomeCurso']} ');
                     window.location.href='novoUsuario.php';</script>";
             }
-        }
     }
