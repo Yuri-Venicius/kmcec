97e3c89c00a45cc05c244ab0efc5dab90fd51a3d
Yuri Oliveira <yvenicios@gmail.com>
2023-02-17 21:34:29 -0300

    Add Aula 15-02 RLM Igor Brasil -- TJMT PRE

--- PrincipalTJ-MTPre.php
+++ PrincipalTJ-MTPre.php
@@ -438,7 +438,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseRlmIgor">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="TJMT_PRE_RLM_IGOR_AULA01.php">AULA 01 - GRAVAÇÃO 15/02/2023</a>
                                     </div>
                                 </div>
                             </div>
