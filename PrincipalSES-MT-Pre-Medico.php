344442ba14a57e2e7fa5dcc77526dcca90b8b052
Yuri Oliveira <yvenicios@gmail.com>
2023-03-11 14:03:58 -0300

    Add aula 08-03 ESTATUTO CLEIDE-- SES PRE 23 TODOS

--- PrincipalSES-MT-Pre-Medico.php
+++ PrincipalSES-MT-Pre-Medico.php
@@ -410,7 +410,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseEstServCleide">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SES_PRE_2023_TODOS_ESTATUTO_CLEIDE_AULA01.php">AULA 01 - GRAVAÇÃO 08/03/2023</a>
                                     </div>
                                 </div>
                             </div>
