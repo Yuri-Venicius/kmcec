3e6ad2a861ff3c5a69a33e5ecf45f57da2844ed8
Yuri Oliveira <yvenicios@gmail.com>
2023-03-14 22:25:55 -0300

    Add aula 11-03 Negociações e vendas -- BB

--- PrincipalBancoDoBrasil.php
+++ PrincipalBancoDoBrasil.php
@@ -363,6 +363,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="BANCO_DO_BRASIL_VENDAS_DAVOLI_AULA01.php">AULA 01 - GRAVAÇÃO 04/03/2023</a>
                                         <a href="BANCO_DO_BRASIL_VENDAS_DAVOLI_AULA02.php">AULA 02 - GRAVAÇÃO 10/03/2023</a>
+                                        <a href="BANCO_DO_BRASIL_VENDAS_DAVOLI_AULA03.php">AULA 03 - GRAVAÇÃO 11/03/2023</a>
                                     </div>
                                 </div>
                             </div>
