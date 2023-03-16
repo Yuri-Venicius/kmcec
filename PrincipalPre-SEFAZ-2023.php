c697770d0d6cbbee550084736ddab1852ceddbbf
Yuri Oliveira <yvenicios@gmail.com>
2023-03-16 10:00:17 -0300

    Add aula 15-03 Dir Adm -- SEFAZ PÓS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -354,7 +354,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseDirCleide">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA01.php">AULA 01 - GRAVAÇÃO 15/03/2022</a>
                                     </div>
                                 </div>
                             </div>
