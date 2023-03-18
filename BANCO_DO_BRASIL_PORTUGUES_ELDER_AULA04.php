4c9d25dc410771ab01302d8ddb6661c208fcbe73
Yuri Oliveira <yvenicios@gmail.com>
2023-03-17 23:45:22 -0300

    Add aula 16-03 PORTUGUES -- BB

--- BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA04.php
+++ BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA04.php
@@ -91,12 +91,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>CURSO PARA O BANCO DO BRASIL - PORTUGUÊS | PROFESSOR ELDER DENCATI</h2><h3>AULA 03</h3>
+                <h2>CURSO PARA O BANCO DO BRASIL - PORTUGUÊS | PROFESSOR ELDER DENCATI</h2><h3>AULA 04</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA02.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA04.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA03.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_ELDER_AULA05.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://drive.google.com/file/d/1rWYqk9CE7nkTis_u3ATsb4PkKqgc_Oxx/preview" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://drive.google.com/file/d/1g63YmcIUZl_yFU_8zNZRkbejpETp8Ind/preview" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 
