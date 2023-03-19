e31fe8e53cc7178e02605b94eb2ad91e6339f8d7
Yuri Oliveira <yvenicios@gmail.com>
2023-03-19 14:35:50 -0300

    Add aula 18-03 Dir Civil -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -394,7 +394,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseDirStanley">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SEFAZ_POS_DIR_CIVIL_STANLEY_AULA01.php">AULA 01 - GRAVAÇÃO 18/03/2022</a>
                                     </div>
                                 </div>
                             </div>
