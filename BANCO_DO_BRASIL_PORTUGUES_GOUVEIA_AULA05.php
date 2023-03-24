581595a3434ad0e4436870cf8de2de1beb52550c
Yuri Oliveira <yvenicios@gmail.com>
2023-03-24 14:39:37 -0300

    Add aula 21-03 Portugues Gouveia -- BB

--- BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA05.php
+++ BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA05.php
@@ -91,12 +91,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>CURSO PARA O BANCO DO BRASIL - PORTUGUÊS | PROFESSOR GOUVEIA</h2><h3>AULA 04</h3>
+                <h2>CURSO PARA O BANCO DO BRASIL - PORTUGUÊS | PROFESSOR GOUVEIA</h2><h3>AULA 05</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA03.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA05.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA04.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA06.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://drive.google.com/file/d/14u6yuPzWpU-IYZaq1yQSmfbFSpJOgrE3/preview" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://drive.google.com/file/d/15Tc4EWmh1wlYJZ71Zl7WmM4a4Qg1y1TL/preview" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 
--- PrincipalBancoDoBrasil.php
+++ PrincipalBancoDoBrasil.php
@@ -236,6 +236,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA02.php">AULA 02 - GRAVAÇÃO 15/02/2023</a>
                                         <a href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA03.php">AULA 03 - GRAVAÇÃO 07/03/2023</a>
                                         <a href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA04.php">AULA 04 - GRAVAÇÃO 14/03/2023</a>
+                                        <a href="BANCO_DO_BRASIL_PORTUGUES_GOUVEIA_AULA05.php">AULA 05 - GRAVAÇÃO 21/03/2023</a>
                                     </div>
                                 </div>
                             </div>
