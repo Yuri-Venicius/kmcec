6cdd4df04c9a5148168d5bab3608dad918f6cbe6
Yuri Oliveira <yvenicios@gmail.com>
2023-03-27 10:21:46 -0300

    Add aula 17-03 Geografia -- SES TODOS

--- PrincipalSES-MT-Pre-Enf-TecEnf.php
+++ PrincipalSES-MT-Pre-Enf-TecEnf.php
@@ -337,7 +337,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseGeoMarcelo">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SES_PRE_2023_TODOS_GEO_MARCELO_AULA01.php">AULA 01 - GRAVAÇÃO 17/03/2023</a>
                                     </div>
                                 </div>
                             </div>
