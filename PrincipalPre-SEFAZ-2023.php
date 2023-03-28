dded8592059a7330c0d7d33726eaca858029297b
Yuri Oliveira <yvenicios@gmail.com>
2023-03-28 17:04:29 -0300

    Add aula 27-03 Direito Adm -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -358,6 +358,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseDirCleide">
                                     <div class="card card-body">
                                         <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA01.php">AULA 01 - GRAVAÇÃO 15/03/2023</a>
+                                        <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA02.php">AULA 02 - GRAVAÇÃO 27/03/2023</a>
                                     </div>
                                 </div>
                             </div>
