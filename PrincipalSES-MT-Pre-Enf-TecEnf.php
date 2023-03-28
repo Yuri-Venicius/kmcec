190b0a1f70cdc6a5a5a8bc389635654dc84c97ef
Yuri Oliveira <yvenicios@gmail.com>
2023-03-28 15:34:45 -0300

    Add aula 27-03 RLM -- SES TODOS

--- PrincipalSES-MT-Pre-Enf-TecEnf.php
+++ PrincipalSES-MT-Pre-Enf-TecEnf.php
@@ -258,6 +258,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseRlmIgor">
                                     <div class="card card-body">
                                         <a href="SES_PRE_2023_TODOS_RLM_IGOR_AULA01.php">AULA 01 - GRAVAÇÃO 10/03/2023</a>
+                                        <a href="SES_PRE_2023_TODOS_RLM_IGOR_AULA02.php">AULA 02 - GRAVAÇÃO 27/03/2023</a>
                                     </div>
                                 </div>
                             </div>
