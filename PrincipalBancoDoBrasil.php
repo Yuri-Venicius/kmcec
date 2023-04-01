6d74c6a84a47967fd94c4bdb8062bf9a0ddf1afd
Yuri Oliveira <yvenicios@gmail.com>
2023-04-01 18:58:06 -0300

    .

--- PrincipalBancoDoBrasil.php
+++ PrincipalBancoDoBrasil.php
@@ -371,12 +371,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="card-body">
                                 <h5 class="card-title">INFORMÁTICA - SILVIO BONONI</h5>
                                 <p class="card-text">
-                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseInfoJoao" aria-expanded="false" aria-controls="collapseExample">
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseInfoSilvio" aria-expanded="false" aria-controls="collapseExample">
                                         <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
                                     </button>
                                     <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1I_8a2ig8iqK9xuOi-jC7adUvxhsFUYZF?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
                                 </p>
-                                <div class="collapse" id="collapseInfoJoao">
+                                <div class="collapse" id="collapseInfoSilvio">
                                     <div class="card card-body">
                                         <a href="BANCO_DO_BRASIL_INFORMATICA_SILVIO_AULA01.php">AULA 01 - GRAVAÇÃO 31/03/2023</a>
                                     </div>
