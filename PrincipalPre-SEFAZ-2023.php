e01322147cf2260ec3e8d46ab07ab288558b589a
Yuri Oliveira <yvenicios@gmail.com>
2023-04-10 12:50:27 -0300

    Add aula 06-04 Economia -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -194,7 +194,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseEconoFandoni">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SEFAZ_POS_ECONOMIA_FANTONI_AULA01.php">AULA 01 - GRAVAÇÃO 06/04/2023</a>
                                     </div>
                                 </div>
                             </div>
