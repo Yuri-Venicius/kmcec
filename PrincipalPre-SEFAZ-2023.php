99fb92f41a0e04d24887873b120233a5c58303dd
Yuri Oliveira <yvenicios@gmail.com>
2023-04-12 18:33:45 -0300

    Add aula 11-04 Direito Administrativo -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -365,6 +365,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA01.php">AULA 01 - GRAVAÇÃO 15/03/2023</a>
                                         <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA02.php">AULA 02 - GRAVAÇÃO 27/03/2023</a>
                                         <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA03.php">AULA 03 - GRAVAÇÃO 08/04/2023</a>
+                                        <a href="SEFAZ_POS_DIR_ADM_CLEIDE_AULA04.php">AULA 04 - GRAVAÇÃO 11/04/2023</a>
                                     </div>
                                 </div>
                             </div>
