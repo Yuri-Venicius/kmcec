265008d556701a723c84a741e59c15c6568f0fd6
Yuri Oliveira <yvenicios@gmail.com>
2023-04-13 16:34:44 -0300

    Add aula 12-04 Atualidades -- BB

--- PrincipalBancoDoBrasil.php
+++ PrincipalBancoDoBrasil.php
@@ -336,8 +336,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseAtualidadesDavoli">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
-
+                                        <a href="BANCO_DO_BRASIL_ATUALIDADES_DAVOLI_AULA01.php">AULA 01 - GRAVAÇÃO 12/04/2023</a>
                                     </div>
                                 </div>
                             </div>
