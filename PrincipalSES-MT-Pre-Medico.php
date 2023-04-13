048caea6bfc5cbbbdcc3eb49691bbb028ede99bd
Yuri Oliveira <yvenicios@gmail.com>
2023-04-13 09:02:55 -0300

    Add Aula 10-04 RLM -- SES TODOS

--- PrincipalSES-MT-Pre-Medico.php
+++ PrincipalSES-MT-Pre-Medico.php
@@ -260,6 +260,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="SES_PRE_2023_TODOS_RLM_IGOR_AULA01.php">AULA 01 - GRAVAÇÃO 10/03/2023</a>
                                         <a href="SES_PRE_2023_TODOS_RLM_IGOR_AULA02.php">AULA 02 - GRAVAÇÃO 27/03/2023</a>
+                                        <a href="SES_PRE_2023_TODOS_RLM_IGOR_AULA03.php">AULA 03 - GRAVAÇÃO 10/04/2023</a>
                                     </div>
                                 </div>
                             </div>
--- SES_PRE_2023_TODOS_RLM_IGOR_AULA03.php
+++ SES_PRE_2023_TODOS_RLM_IGOR_AULA03.php
@@ -91,12 +91,11 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>CURSO PARA A SES-MT PRÉ-EDITAL - RACIOCÍNIO LÓGICO | PROFESSOR IGOR BRASIL</h2><h3>AULA 02</h3>
+                <h2>CURSO PARA A SES-MT PRÉ-EDITAL - RACIOCÍNIO LÓGICO | PROFESSOR IGOR BRASIL</h2><h3>AULA 03</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_PRE_2023_TODOS_RLM_IGOR_AULA01.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_PRE_2023_TODOS_RLM_IGOR_AULA03.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_PRE_2023_TODOS_RLM_IGOR_AULA02.php">Anterior</a>
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/krv5cq" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/ubqq0r" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
