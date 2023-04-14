d741c743267189fac1bf7855c4ecbac79f448749
Yuri Oliveira <yvenicios@gmail.com>
2023-04-14 17:55:59 -0300

    Add aula 13-04 Dir Tributario -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -302,6 +302,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="SEFAZ_POS_DIR_TRIBUTARIO_LIGIA_AULA01.php">AULA 01 - GRAVAÇÃO 30/03/2023</a>
                                         <a href="SEFAZ_POS_DIR_TRIBUTARIO_LIGIA_AULA02.php">AULA 02 - GRAVAÇÃO 03/04/2023</a>
+                                        <a href="SEFAZ_POS_DIR_TRIBUTARIO_LIGIA_AULA03.php">AULA 03 - GRAVAÇÃO 13/04/2023</a>
                                     </div>
                                 </div>
                             </div>
