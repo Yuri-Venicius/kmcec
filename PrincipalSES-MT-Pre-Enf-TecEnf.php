95c6b6170080a0d8f3d041859320d9207d48a5e8
Yuri Oliveira <yvenicios@gmail.com>
2023-04-14 20:35:11 -0300

    Add aula 13-04 Redaçao -- SES TODOS

--- PrincipalSES-MT-Pre-Enf-TecEnf.php
+++ PrincipalSES-MT-Pre-Enf-TecEnf.php
@@ -250,6 +250,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
+                                <h5 class="card-title">REDAÇÃO - KELLEN</h5>
+                                <p class="card-text">
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseRedacaoKellen" aria-expanded="false" aria-controls="collapseExample">
+                                        <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                    </button>
+                                    <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1jhNDj7Ro8o26cuYZFj2N_khFVwJDcyK9?usp=share_link" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                                </p>
+                                <div class="collapse" id="collapseRedacaoKellen">
+                                    <div class="card card-body">
+                                        <a href="SES_PRE_TODOS_REDACAO_KELLEN_AULA01.php">AULA 01 - GRAVAÇÃO 13/04/2023</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p></p>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
                                 <h5 class="card-title">RACIOCÍNIO LÓGICO MATEMÁTICO - IGOR BRASIL</h5>
                                 <p class="card-text">
                                     <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseRlmIgor" aria-expanded="false" aria-controls="collapseExample">
