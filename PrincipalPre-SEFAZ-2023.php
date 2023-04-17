42198f32041c476347c036477910d1aa1f3aa45b
Yuri Oliveira <yvenicios@gmail.com>
2023-04-17 12:05:35 -0300

    Add aula 15-04 Manha Estatistica --SEFAZ

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -558,6 +558,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="SEFAZ_POS_ESTATISTICA_IGOR_AULA01.php">AULA 01 - GRAVAÇÃO 08/04/2023</a>
                                         <a href="SEFAZ_POS_ESTATISTICA_IGOR_AULA02.php">AULA 02 - GRAVAÇÃO 09/04/2023</a>
+                                        <a href="SEFAZ_POS_ESTATISTICA_IGOR_AULA03.php">AULA 03 - GRAVAÇÃO 15/04/2023</a>
                                     </div>
                                 </div>
                             </div>
--- SEFAZ_POS_ESTATISTICA_IGOR_AULA03.php
+++ SEFAZ_POS_ESTATISTICA_IGOR_AULA03.php
@@ -91,12 +91,11 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>CURSO PARA A SEFAZ-MT PÓS EDITAL - ESTATÍSTICA | PROFESSOR IGOR BRASIL</h2><h3>AULA 02</h3>
+                <h2>CURSO PARA A SEFAZ-MT PÓS EDITAL - ESTATÍSTICA | PROFESSOR IGOR BRASIL</h2><h3>AULA 03</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEFAZ_POS_ESTATISTICA_IGOR_AULA01.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEFAZ_POS_ESTATISTICA_IGOR_AULA03.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SEFAZ_POS_ESTATISTICA_IGOR_AULA02.php">Anterior</a>
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/o9mqve" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/lvg1r8" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
