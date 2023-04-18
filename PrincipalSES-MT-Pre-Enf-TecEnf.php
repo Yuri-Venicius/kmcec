a9c1eed389dc2f357d24f55ab3ed2b557ed80223
Yuri Oliveira <yvenicios@gmail.com>
2023-04-18 00:41:42 -0300

    Add Aula 11-04 Portugues Elder -- SES TODOS

--- PrincipalSES-MT-Pre-Enf-TecEnf.php
+++ PrincipalSES-MT-Pre-Enf-TecEnf.php
@@ -197,7 +197,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapsePtElder">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SES_PRE_2023_TODOS_PORTUGUES_ELDER_AULA01.php">AULA 01 - GRAVAÇÃO 11/04/2023</a>
                                     </div>
                                 </div>
                             </div>
