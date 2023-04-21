9015c80e8d20274e9ace828605615911d316df43
Yuri Oliveira <yvenicios@gmail.com>
2023-04-21 20:21:24 -0300

    Add aula 20-04 T.I JOAO CLEBER -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -302,6 +302,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseTiJoao">
                                     <div class="card card-body">
                                         <a href="SEFAZ_POS_TI_JOAO_AULA01.php">AULA 01 - GRAVAÇÃO 17/04/2023</a>
+                                        <a href="SEFAZ_POS_TI_JOAO_AULA02.php">AULA 02 - GRAVAÇÃO 20/04/2023</a>
                                     </div>
                                 </div>
                             </div>
