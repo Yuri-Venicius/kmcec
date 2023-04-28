baa7e56865bdbac8cc3683c2977f4c9a5ce916ae
Yuri Oliveira <yvenicios@gmail.com>
2023-04-27 23:41:26 -0300

    Add aual 17-04 Direito Adm -- TJMT PRE

--- PrincipalTJ-MTPre.php
+++ PrincipalTJ-MTPre.php
@@ -296,6 +296,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="TJMT_PRE_DIR_ADM_CLEIDE_AULA02.php">AULA 02 - GRAVAÇÃO 27/02/2023</a>
                                         <a href="TJMT_PRE_DIR_ADM_CLEIDE_AULA03.php">AULA 03 - GRAVAÇÃO 10/04/2023</a>
                                         <a href="TJMT_PRE_DIR_ADM_CLEIDE_AULA04.php">AULA 04 - GRAVAÇÃO 12/04/2023</a>
+                                        <a href="TJMT_PRE_DIR_ADM_CLEIDE_AULA05.php">AULA 05 - GRAVAÇÃO 17/04/2023</a>
                                     </div>
                                 </div>
                             </div>
