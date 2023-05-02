7dde671badc282c515ab90110047be172494b072
Yuri Oliveira <yvenicios@gmail.com>
2023-05-02 09:25:08 -0300

    Add aula 28-04 Direito Const -- TJMT PRE

--- PrincipalTJ-MTPre.php
+++ PrincipalTJ-MTPre.php
@@ -273,6 +273,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="TJMT_PRE_DIR_CONST_JOSE_AULA03.php">AULA 03 - GRAVAÇÃO 28/02/2023</a>
                                         <a href="TJMT_PRE_DIR_CONST_JOSE_AULA04.php">AULA 04 - GRAVAÇÃO 03/03/2023</a>
                                         <a href="TJMT_PRE_DIR_CONST_JOSE_AULA05.php">AULA 05 - GRAVAÇÃO 10/03/2023</a>
+                                        <a href="TJMT_PRE_DIR_CONST_JOSE_AULA06.php">AULA 06 - GRAVAÇÃO 28/04/2023</a>
                                     </div>
                                 </div>
                             </div>
