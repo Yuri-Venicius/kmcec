0d023eaac1eb6b02c19622bd539ee874ac3563b6
Yuri Oliveira <yvenicios@gmail.com>
2023-05-06 13:50:14 -0300

    Add aula 05-05 Auditoria -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -505,6 +505,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseAuditoriaTonyvan">
                                     <div class="card card-body">
                                         <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA01.php">AULA 01 - GRAVAÇÃO 04/05/2023</a>
+                                        <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA02.php">AULA 02 - GRAVAÇÃO 05/05/2023</a>
                                     </div>
                                 </div>
                             </div>
