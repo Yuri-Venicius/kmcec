3c2f323f892863c40f67edc66d58d357ca07f04b
Yuri Oliveira <yvenicios@gmail.com>
2023-05-12 22:26:46 -0300

    Add aula extra Auditoria -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -512,6 +512,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA03.php">AULA 03 - GRAVAÇÃO 06/05/2023</a>
                                         <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA04.php">AULA 04 - GRAVAÇÃO 06/05/2023</a>
                                         <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA05.php">AULA 05 - GRAVAÇÃO 07/05/2023</a>
+                                        <a href="SEFAZ_POS_AUDITORIA_TONYVAN_AULA06.php">AULA EXTRA </a>
                                     </div>
                                 </div>
                             </div>
