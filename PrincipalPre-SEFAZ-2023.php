a4c0c78cf73962dde9637c71cdb1d9726515e361
Yuri Oliveira <yvenicios@gmail.com>
2023-05-18 12:44:35 -0300

    Add aula 16-05 Dir Empresarial -- SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -406,7 +406,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseDirEmpreJose">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="SEFAZ_POS_DIR_EMPRESARIAL_JOSE_AULA01.php">AULA 01 - GRAVAÇÃO 16/05/2023</a>
                                     </div>
                                 </div>
                             </div>
