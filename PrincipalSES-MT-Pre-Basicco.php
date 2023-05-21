1d4257c7d1bc7c54e298a6d8a291da26eb7edeb5
Yuri Oliveira <yvenicios@gmail.com>
2023-05-21 12:41:20 -0300

    Add Aula 20-05 Leglisação Sus -- SES TODOS

--- PrincipalSES-MT-Pre-Basicco.php
+++ PrincipalSES-MT-Pre-Basicco.php
@@ -396,6 +396,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SES_PRE_2023_TODOS_LEG_SUS_DAVOLI_AULA04.php">AULA 04</a>
                                         <a href="SES_PRE_2023_TODOS_LEG_SUS_DAVOLI_AULA05.php">AULA 05</a>
                                         <a href="SES_PRE_2023_TODOS_LEG_SUS_DAVOLI_AULA06.php">AULA 06</a>
+                                        <a href="SES_PRE_2023_TODOS_LEG_SUS_DAVOLI_AULA07.php">AULA 07</a>
                                     </div>
                                 </div>
                             </div>
