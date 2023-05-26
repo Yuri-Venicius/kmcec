90da8e526543231cd64469429963c96321fadb39
Yuri Oliveira <yvenicios@gmail.com>
2023-05-26 11:07:22 -0300

    Add aula 23-05 Etica -- TJMT PRE

--- PrincipalTJ-MTPre.php
+++ PrincipalTJ-MTPre.php
@@ -649,7 +649,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseEticaDouglas">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="TJMT_PRE_ETICA_DOUGLAS_AULA01.php">AULA 01 - GRAVAÇÃO 23/05/2023</a>
                                     </div>
                                 </div>
                             </div>
