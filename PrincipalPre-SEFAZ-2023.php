cb89b65fa6ff75b94ca0fd3b8be819892605baac
Yuri Oliveira <yvenicios@gmail.com>
2023-05-30 16:01:50 -0300

    Add aula 29-05 SEFAZ POS

--- PrincipalPre-SEFAZ-2023.php
+++ PrincipalPre-SEFAZ-2023.php
@@ -183,7 +183,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div>  
                         <p></p>
                     </div>
 
@@ -203,6 +203,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SEFAZ_POS_ECONOMIA_FANTONI_AULA02.php">AULA 02 - GRAVAÇÃO 12/04/2023</a>
                                         <a href="SEFAZ_POS_ECONOMIA_FANTONI_AULA03.php">AULA 03 - GRAVAÇÃO 12/05/2023</a>
                                         <a href="SEFAZ_POS_ECONOMIA_FANTONI_AULA04.php">AULA 04 - GRAVAÇÃO 24/05/2023</a>
+                                        <a href="SEFAZ_POS_ECONOMIA_FANTONI_AULA05.php">AULA 05 - GRAVAÇÃO 29/05/2023</a>
                                     </div>
                                 </div>
                             </div>
