bb24c0ed40e7079a27cb0c7ec9ac5a857b9e9b14
Yuri Oliveira <yvenicios@gmail.com>
2023-06-02 07:58:27 -0300

    Add aula 01-06 Dir Adm -- GCM LRV

--- PrincipalLucasRioVerdeGM.php
+++ PrincipalLucasRioVerdeGM.php
@@ -313,7 +313,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="GMLRV_DIREITOADM_CLEIDE_AULA01.php">AULA 01 - GRAVAÇÃO 05/05/2023</a>
                                         <a href="GMLRV_DIREITOADM_CLEIDE_AULA02.php">AULA 02 - GRAVAÇÃO 09/05/2023</a>
                                         <a href="GMLRV_DIREITOADM_CLEIDE_AULA03.php">AULA 03 - GRAVAÇÃO 29/05/2023</a>
-                                        <a href="GMLRV_DIREITOADM_CLEIDE_AULA03.php">AULA 03 - GRAVAÇÃO 29/05/2023</a>
+                                        <a href="GMLRV_DIREITOADM_CLEIDE_AULA04.php">AULA 04 - GRAVAÇÃO 01/06/2023</a>
                                     </div>
                                 </div>
                             </div>
