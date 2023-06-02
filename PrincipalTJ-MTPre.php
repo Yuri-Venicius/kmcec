14075efd2650588b1b2141314a6eb0ffc90bca24
Yuri Oliveira <yvenicios@gmail.com>
2023-06-02 10:04:30 -0300

    Add aula 31-05 Proc Civil TJMT PRE

--- PrincipalTJ-MTPre.php
+++ PrincipalTJ-MTPre.php
@@ -346,7 +346,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 </p>
                                 <div class="collapse" id="collapseDirProcStanley">
                                     <div class="card card-body">
-                                        <a>NÃO HÁ CONTEÚDO GRAVADO</a>
+                                        <a href="TJMT_PRE_DIR_PROC_CIVIL_STANLEY_AULA01.php">AULA 01 - GRAVAÇÃO 31/05/2023</a>
                                     </div>
                                 </div>
                             </div>
