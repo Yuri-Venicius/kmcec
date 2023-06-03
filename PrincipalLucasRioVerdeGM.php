62406a72abf2826814d1e02f86540eee5d03afd2
Yuri Oliveira <yvenicios@gmail.com>
2023-06-03 12:50:11 -0300

    Add aula 02-06 Informatica -- GCM LRV

--- PrincipalLucasRioVerdeGM.php
+++ PrincipalLucasRioVerdeGM.php
@@ -268,6 +268,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="GMLRV_INFORMATICA_JOAO_AULA01.php">AULA 01 - GRAVAÇÃO 04/05/2023</a>
                                         <a href="GMLRV_INFORMATICA_JOAO_AULA02.php">AULA 02 - GRAVAÇÃO 11/05/2023</a>
+                                        <a href="GMLRV_INFORMATICA_JOAO_AULA03.php">AULA 03 - GRAVAÇÃO 02/06/2023</a>
                                     </div>
                                 </div>
                             </div>
