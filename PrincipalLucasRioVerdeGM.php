fef5caf2b9eb91d9540f9f38feb02c86f59a83e8
Yuri Oliveira <yvenicios@gmail.com>
2023-06-07 00:54:49 -0300

    Add aula 06-06 GCM LRV

--- PrincipalLucasRioVerdeGM.php
+++ PrincipalLucasRioVerdeGM.php
@@ -183,6 +183,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="GMLRV_PORTUGUES_ELDER_AULA01.php">AULA 01 - GRAVAÇÃO 10/05/2023</a>
                                         <a href="GMLRV_PORTUGUES_ELDER_AULA02.php">AULA 02 - GRAVAÇÃO 27/05/2023</a>
+                                        <a href="GMLRV_PORTUGUES_ELDER_AULA03.php">AULA 03 - GRAVAÇÃO 06/06/2023</a>
                                     </div>
                                 </div>
                             </div>
