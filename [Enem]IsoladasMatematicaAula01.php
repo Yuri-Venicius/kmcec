e09da06c3797a8606cee9b32e5eb221241a4c78c
Yuri Oliveira <yvenicios@gmail.com>
2023-08-09 17:34:55 -0300

    Add aula 02 Matematica Isoladas ENEM

--- [Enem]IsoladasMatematicaAula01.php
+++ [Enem]IsoladasMatematicaAula01.php
@@ -88,7 +88,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
                 <h2>ISOLADAS DO KM PARA ENEM - MATEMÁTICA | PROFESSOR IGOR BRASIL</h2><h3>AULA 01</h3>
                 <div>
-                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasMatematicaAula02.php">Próximo</a> -->
+                <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasMatematicaAula02.php">Próximo</a>
                 <p></p>
                 <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/4j8NIMDcwXQ" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
