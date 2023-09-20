dfa22bd536707999343cd5ac3ad50f4a99a6d954
Yuri Oliveira <yvenicios@gmail.com>
2023-09-20 16:37:16 -0300

    Add aula 18-09 Matematica -- Isoladas do Enem

--- PrincipalIsoladasEnemMatematica2023.php
+++ PrincipalIsoladasEnemMatematica2023.php
@@ -139,6 +139,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="[Enem]IsoladasMatematicaAula05.php">AULA 05 - 28/08/2023</a><p></p> 
                                         <a href="[Enem]IsoladasMatematicaAula06.php">AULA 06 - 04/09/2023</a><p></p> 
                                         <a href="[Enem]IsoladasMatematicaAula07.php">AULA 07 - 10/09/2023</a><p></p> 
+                                        <a href="[Enem]IsoladasMatematicaAula08.php">AULA 08 - 18/09/2023</a><p></p> 
                                     </div>
                                     </div>    
                         </div>
