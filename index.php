b024ec6ab2642523378ac43ac0f45aafea05bc71
Yuri Oliveira <yvenicios@gmail.com>
2023-10-11 13:03:15 -0300

    add aulas e curso GCM VG 2023

--- index.php
+++ index.php
@@ -149,6 +149,28 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                 <?php endif; ?>
 
                 <?php
+                $codCurso = 3587;
+                $userTemp = verificaAcessoAoCurso($conexao, $usuarioLogado['id'], $codCurso);
+                if (
+                    $verificaAdm['nivelAcesso'] == '1' || ($userTemp['idUsuario'] == $usuarioLogado['id'] && $userTemp['idCurso'] == $codCurso && $userTemp['statusAcesso'] == 'ATIVO')
+                ) : ?>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Curso Pré edital Para o Concurso Guarda Municipal de VG [Online]</h5>
+                                <p class="card-text">Curso Pré edital Para o Concurso Guarda Municipal de VG [Online].
+                                </p>
+                                </p>
+                                <p></p>
+                                <a href="PrincipalGcmVgPre2023.php" class="btn btn-primary">Acessar</a>
+                                <!-- <a style="margin-left: 30px;" href="" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a> -->
+                            </div>
+                        </div>
+                        <p></p>
+                    </div>
+                <?php endif; ?>
+
+                <?php
                 $codCurso = 3574;
                 $userTemp = verificaAcessoAoCurso($conexao, $usuarioLogado['id'], $codCurso);
                 if (
