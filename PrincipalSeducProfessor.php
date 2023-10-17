0a8b439e1c0d63b9b9706fa68c028452399bd1a9
Yuri Oliveira <yvenicios@gmail.com>
2023-10-17 18:35:50 -0300

    Add aula 16-10 SEDUC

--- PrincipalSeducProfessor.php
+++ PrincipalSeducProfessor.php
@@ -165,6 +165,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="collapse" id="collapseRlmIgor">
                                 <div class="card card-body">
                                     <a href="SEDUC_RLM_IGOR_AULA01.php">AULA 01</a>
+                                    <a href="SEDUC_RLM_IGOR_AULA02.php">AULA 02</a>
                                 </div>
                             </div>
                         </div>
--- SEDUC_RLM_IGOR_AULA02.php
+++ SEDUC_RLM_IGOR_AULA02.php
@@ -85,11 +85,11 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             <!-- Page Content  -->
             <div id="content">
-                <h2>CURSO PARA SEDUC 2023 - RLM | PROFESSOR IGOR BRASIL</h2><h3>AULA 01</h3>
+                <h2>CURSO PARA SEDUC 2023 - RLM | PROFESSOR IGOR BRASIL</h2><h3>AULA 02</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA02.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA01.php">Anterior</a>
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/oIwlu_Nx4lM" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/avFXVij0e-0" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
