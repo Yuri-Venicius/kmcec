48eb5f1394701cfdc3d7b8cfb1c4e92e30d5b387
Yuri Oliveira <yvenicios@gmail.com>
2023-10-17 11:06:24 -0300

    refatorando

--- PrincipalSeducProfessor.php
+++ PrincipalSeducProfessor.php
@@ -117,40 +117,39 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                 Para acessar uma aula basta clicar em "Aulas" no bloco do conteúdo referente.</p>
 
             <div class="row">
-                
                 <div class="col-sm-12">
-                    <h3>CONTEÚDO BÔNUS</h3>
-                    <?php if (
-                        $_SESSION['usuario'] == 'yvenicios@gmail.com' || $_SESSION['usuario'] == 'kleybermoreno@gmail.com' || $_SESSION['usuario'] == 'joaovitormoreno01@gmail.com' || $_SESSION['usuario'] == 'kedmasantosmoreno@gmail.com'
-                    ) : ?>
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">REDAÇÃO UFMT - HÉLIO TAQUES</h5>
-                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseRedacaoHelio" aria-expanded="false" aria-controls="collapseExample">
-                                        <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
-                                    </button>
-                                    <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1PuN_yrINm9mAblGJh32GrfIgSa_NZRwX?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
-                                    </p>
-                                    <div class="collapse" id="collapseRedacaoHelio">
-                                        <div class="card card-body">
-                                            <a href="REDACAO_UFMT_HELIO_AULA01.php">AULA 01 - GRAVAÇÃO 16/01/2022</a>
-                                            <p></p>
-                                            <a href="REDACAO_UFMT_HELIO_AULA02.php">AULA 02 - GRAVAÇÃO 23/01/2022</a>
-                                            <p></p>
-                                            <a href="REDACAO_UFMT_HELIO_AULA03.php">AULA 03 - GRAVAÇÃO 30/01/2022</a>
-                                            <p></p>
-                                            <a href="REDACAO_UFMT_HELIO_AULA04.php">AULA 04 - GRAVAÇÃO 13/02/2022</a>
-                                            <p></p>
-                                            <a href="POS-DICAS_DAVOLI_AULA01.php">AULA ÚNICA - GRAVAÇÃO 13/02/2022</a>
-                                            <p></p>
-                                        </div>
-                                    </div>
+                    <h3>Conteúdo Bônus: </h3>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">REDAÇÃO UFMT - HÉLIO TAQUES</h5>
+                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseRedacaoHelio" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                            </button>
+                            <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1PuN_yrINm9mAblGJh32GrfIgSa_NZRwX?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                            </p>
+                            <div class="collapse" id="collapseRedacaoHelio">
+                                <div class="card card-body">
+                                    <a href="REDACAO_UFMT_HELIO_AULA01.php">AULA 01 - GRAVAÇÃO 16/01/2022</a>
+                                    <p></p>
+                                    <a href="REDACAO_UFMT_HELIO_AULA02.php">AULA 02 - GRAVAÇÃO 23/01/2022</a>
+                                    <p></p>
+                                    <a href="REDACAO_UFMT_HELIO_AULA03.php">AULA 03 - GRAVAÇÃO 30/01/2022</a>
+                                    <p></p>
+                                    <a href="REDACAO_UFMT_HELIO_AULA04.php">AULA 04 - GRAVAÇÃO 13/02/2022</a>
+                                    <p></p>
+                                    <a href="POS-DICAS_DAVOLI_AULA01.php">AULA ÚNICA - GRAVAÇÃO 13/02/2022</a>
+                                    <p></p>
                                 </div>
                             </div>
-                            <p></p>
                         </div>
-                    <?php endif; ?>
+                    </div>
+                    <p></p>
+                </div>
+
+                <div class="col-md-12">
+                    <h3>Conteúdo do curso: </h3>
                 </div>
 
                 <div class="col-sm-6">
