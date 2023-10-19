fe3e35d7ec4a10fe92064102de7aa90d465404c5
Yuri Oliveira <yvenicios@gmail.com>
2023-10-19 11:27:46 -0300

    Add aula 17-10 RLM - SEDUC

--- PrincipalSeducProfessor.php
+++ PrincipalSeducProfessor.php
@@ -166,6 +166,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="card card-body">
                                     <a href="SEDUC_RLM_IGOR_AULA01.php">AULA 01</a>
                                     <a href="SEDUC_RLM_IGOR_AULA02.php">AULA 02</a>
+                                    <a href="SEDUC_RLM_IGOR_AULA03.php">AULA 03</a>
                                 </div>
                             </div>
                         </div>
