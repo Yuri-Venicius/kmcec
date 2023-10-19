1f512374bc1d813028ceb99a9bcd2458233a75e4
Yuri Oliveira <yvenicios@gmail.com>
2023-10-19 12:46:17 -0300

    Add aula 18-10 SEDUC

--- SEDUC_INFO_JOAO_AULA02.php
+++ SEDUC_INFO_JOAO_AULA02.php
@@ -85,11 +85,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             <!-- Page Content  -->
             <div id="content">
-                <h2>CURSO PARA SEDUC 2023 - INFORMÁTICA | PROFESSOR JOÃO CLEBER</h2><h3>AULA 01</h3>
+                <h2>CURSO PARA SEDUC 2023 - INFORMÁTICA | PROFESSOR JOÃO CLEBER</h2><h3>AULA 02</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_INFO_JOAO_AULA02.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_INFO_JOAO_AULA01.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_INFO_JOAO_AULA03.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/id8fUWfKQ3s" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/wsuOVtaaAVA" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
