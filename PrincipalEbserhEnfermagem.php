50340f442002c1aa8d414866d1635c819aef7800
Yuri Oliveira <yvenicios@gmail.com>
2023-10-20 11:47:45 -0300

    Add aula 01 Leg Davoli - EBSERH

--- PrincipalEbserhEnfermagem.php
+++ PrincipalEbserhEnfermagem.php
@@ -148,6 +148,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     <p></p>
                 </div>
 
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">LEGISLAÇÃO DO SUS - FERNANDO DAVOLI</h5>
+                            <p class="card-text">
+                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseLegDavoli" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                </button>
+                                <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/11efe9dVOwIiLGdluiR8hDIR8FfuTGp6L?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                            </p>
+                            <div class="collapse" id="collapseLegDavoli">
+                                <div class="card card-body">
+                                    <a href="EBSERH_LEG_DAVOLI_AULA01.php">AULA 01</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p></p>
+                </div>
+
             </div>
 
             <!-- jQuery CDN - Slim version (=without AJAX) -->
