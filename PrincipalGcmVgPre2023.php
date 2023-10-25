61fa60db694d62b573eb5356d2909a5e23a61ded
Yuri Oliveira <yvenicios@gmail.com>
2023-10-25 17:25:05 -0300

    add aula 24-10 PT HELIO -- SEDUC, EBSERH, GCMVG

--- PrincipalGcmVgPre2023.php
+++ PrincipalGcmVgPre2023.php
@@ -230,24 +230,44 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
             </div>
 
             <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">DIREITO CONSTITUCIONAL - JOSÉ NETO</h5>
-                            <p class="card-text">
-                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseDirJoseNeto" aria-expanded="false" aria-controls="collapseExample">
-                                    <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
-                                </button>
-                                <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/14DRjTVgkmqMZolhyPySVszneq-qSFdaM?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
-                            </p>
-                            <div class="collapse" id="collapseDirJoseNeto">
-                                <div class="card card-body">
-                                    <a href="GUARDAVG2023_DIR_CONSTITUCIONAL_JOSE_AULA01.php">AULA 01</a>
-                                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">DIREITO CONSTITUCIONAL - JOSÉ NETO</h5>
+                        <p class="card-text">
+                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseDirJoseNeto" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                            </button>
+                            <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/14DRjTVgkmqMZolhyPySVszneq-qSFdaM?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                        </p>
+                        <div class="collapse" id="collapseDirJoseNeto">
+                            <div class="card card-body">
+                                <a href="GUARDAVG2023_DIR_CONSTITUCIONAL_JOSE_AULA01.php">AULA 01</a>
                             </div>
                         </div>
                     </div>
-                    <p></p>
                 </div>
+                <p></p>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">INTERPRETAÇÃO DE TEXTO - HÉLIO TAQUES</h5>
+                        <p class="card-text">
+                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseInterHelio" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                            </button>
+                            <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/15hJLyJJ8KJ9vbQyWCt44MH42TRYyKjYy?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                        </p>
+                        <div class="collapse" id="collapseInterHelio">
+                            <div class="card card-body">
+                                <a href="GUARDAVG2023_INTERPRETACAO_HELIO_AULA01.php">AULA 01</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p></p>
+            </div>
 
         </div>
 
--- PrincipalSeducProfessor.php
+++ PrincipalSeducProfessor.php
@@ -266,6 +266,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     </div>
                     <p></p>
                 </div>
+
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">INTERPRETAÇÃO DE TEXTO - HÉLIO TAQUES</h5>
+                            <p class="card-text">
+                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseInterHelio" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                </button>
+                                <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1b9xNVQRPeuMsTBAPdzKG399pXS-0kVXz?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                            </p>
+                            <div class="collapse" id="collapseInterHelio">
+                                <div class="card card-body">
+                                    <a href="SEDUC_INTERPRETACAO_HELIO_AULA01.php">AULA 01</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p></p>
+                </div>
                 
             </div>
 
