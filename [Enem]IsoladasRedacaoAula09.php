e91784d5e24565eca2c9ca6f0410c05f0aeb0c52
Yuri Oliveira <yvenicios@gmail.com>
2023-10-28 15:56:21 -0300

    Add aula 27-10 Isoladas Enem - Redação

--- [Enem]IsoladasRedacaoAula09.php
+++ [Enem]IsoladasRedacaoAula09.php
@@ -86,12 +86,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
             <!-- Page Content  -->
             <div id="content">
 
-                <h2>ISOLADAS DO KM PARA ENEM - REDAÇÃO | PROFESSORA KELINA SOUZA</h2><h3>AULA 08</h3>
+                <h2>ISOLADAS DO KM PARA ENEM - REDAÇÃO | PROFESSORA KELINA SOUZA</h2><h3>AULA 09</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasRedacaoAula07.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasRedacaoAula09.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasRedacaoAula08.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="[Enem]IsoladasRedacaoAula10.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/BtLuncdRjkY" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/wuTLXgqPcdA" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
