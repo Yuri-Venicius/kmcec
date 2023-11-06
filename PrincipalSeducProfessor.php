6e35122dbc335d8b753e67b1016fb0f786658410
Yuri Oliveira <yvenicios@gmail.com>
2023-11-06 15:45:15 -0300

    Add Aula 04/11 SEDUC LC 112/02 DAVOLI

--- PrincipalSeducProfessor.php
+++ PrincipalSeducProfessor.php
@@ -363,6 +363,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     <p></p>
                 </div>
 
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">LC 112/02 - FERNANDO DAVOLI</h5>
+                            <p class="card-text">
+                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapsEcaLcCleide" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                </button>
+                                <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/15lx3qs85kR0lJDjKxom_VE6EpIjLR5Rd?usp=sharing" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                            </p>
+                            <div class="collapse" id="collapsEcaLcCleide">
+                                <div class="card card-body">
+                                    <a href="SEDUC_LC11202_DAVOLI_AULA01.php">AULA 01</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p></p>
+                </div>
+
             </div>
 
 
