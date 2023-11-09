fd8fe2efcaac9c540cfad74fef031ac670a7ef87
Yuri Oliveira <yvenicios@gmail.com>
2023-11-09 10:38:05 -0300

    Add aula 08/11 RLM IGOR -- SEDUC

--- SEDUC_RLM_IGOR_AULA05.php
+++ SEDUC_RLM_IGOR_AULA05.php
@@ -85,12 +85,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             <!-- Page Content  -->
             <div id="content">
-                <h2>CURSO PARA SEDUC 2023 - RLM | PROFESSOR IGOR BRASIL</h2><h3>AULA 04</h3>
+                <h2>CURSO PARA SEDUC 2023 - RLM | PROFESSOR IGOR BRASIL</h2><h3>AULA 05</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA03.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA05.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA04.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="SEDUC_RLM_IGOR_AULA06.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/yziwmFh7L30" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/1waCmLWox8g" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
             </div>
