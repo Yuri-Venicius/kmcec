bfbc1be0235eeedc9d3ec6d94e48e693b65ab950
Yuri Oliveira <yvenicios@gmail.com>
2023-11-11 00:52:04 -0300

    ..

--- conexaoPostgres.php
+++ conexaoPostgres.php
@@ -1,9 +1,9 @@
 <?php
 define('PG_HOST', '89.117.33.41');
-define('PG_PORT', '5780');
-define('PG_USER', 'postgres');
-define('PG_PASSWORD', 'prodKM@@agente$%=');
-define('PG_DB', 'km-prod');
+define('PG_PORT', '5432');
+define('PG_USER', 'yuri.oliveira');
+define('PG_PASSWORD', 'yuri@123');
+define('PG_DB', 'km-hml');
 
 try {
     $conexaoPostgres = new PDO("pgsql:host=".PG_HOST.";port=".PG_PORT.";dbname=".PG_DB, PG_USER, PG_PASSWORD);
