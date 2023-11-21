999ecea4059d06eb8b08cf9e8a5e23c94e4c78f7
Yuri Oliveira <yvenicios@gmail.com>
2023-11-21 16:11:09 -0300

    Add aula 14-11 Portugues Helio

--- PrincipalEbserhBasico.php
+++ PrincipalEbserhBasico.php
@@ -217,6 +217,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="collapse" id="collapsePtHelio">
                                 <div class="card card-body">
                                     <a href="EBSERH_PT_HELIO_AULA01.php">AULA 01</a>
+                                    <a href="EBSERH_PT_HELIO_AULA02.php">AULA 02</a>
                                 </div>
                             </div>
                         </div>
