1bb61994d60d6a3b855c16b28a6662b0bcb58b35
Yuri Oliveira <yvenicios@gmail.com>
2023-11-21 23:57:13 -0300

    Add aula 18-11 Leg Ebserh Davoli

--- EBSERH_LEG_DAVOLI_AULA08.php
+++ EBSERH_LEG_DAVOLI_AULA08.php
@@ -88,12 +88,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
 
             
 
-                <h2>EBSERH - LEGISLAÇÃO DO SUS | PROFESSOR FERNANDO DAVOLI</h2><h3>AULA 07</h3>
+                <h2>EBSERH - LEGISLAÇÃO DO SUS | PROFESSOR FERNANDO DAVOLI</h2><h3>AULA 08</h3>
                 <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="EBSERH_LEG_DAVOLI_AULA06.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="EBSERH_LEG_DAVOLI_AULA08.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="EBSERH_LEG_DAVOLI_AULA07.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="EBSERH_LEG_DAVOLI_AULA09.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/qiPuDdgRrXg" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/gznfVc9EMWw" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
                 </div>
                 <p></p>
                 
