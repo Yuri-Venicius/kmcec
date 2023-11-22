66b11895b475d13c5d13f5aa009235b777421f5e
Yuri Oliveira <yvenicios@gmail.com>
2023-11-22 11:01:22 -0300

    Add aula 21-11 Ebserh RLM

--- PrincipalEbserhEnfermagem.php
+++ PrincipalEbserhEnfermagem.php
@@ -188,6 +188,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <a href="EBSERH_RLM_IGOR_AULA03.php">AULA 03</a>
                                     <a href="EBSERH_RLM_IGOR_AULA04.php">AULA 04</a>
                                     <a href="EBSERH_RLM_IGOR_AULA05.php">AULA 05</a>
+                                    <a href="EBSERH_RLM_IGOR_AULA06.php">AULA 06</a>
                                 </div>
                             </div>
                         </div>
