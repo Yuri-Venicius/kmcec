97559e56e7922d9907796efd3218481106faf098
Yuri Oliveira <yvenicios@gmail.com>
2023-11-22 16:26:48 -0300

    Add aula 17/11/23 Dir Const Jose Neto -- GCM VG

--- PrincipalGcmVgPre2023.php
+++ PrincipalGcmVgPre2023.php
@@ -247,6 +247,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="card card-body">
                                     <a href="GUARDAVG2023_DIR_CONSTITUCIONAL_JOSE_AULA01.php">AULA 01</a>
                                     <a href="GUARDAVG2023_DIR_CONSTITUCIONAL_JOSE_AULA02.php">AULA 02</a>
+                                    <a href="GUARDAVG2023_DIR_CONSTITUCIONAL_JOSE_AULA03.php">AULA 03</a>
                                 </div>
                             </div>
                         </div>
