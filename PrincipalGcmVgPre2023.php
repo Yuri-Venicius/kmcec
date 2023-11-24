aa0689d732949a6121e862c109941a3a715e535a
Yuri Oliveira <yvenicios@gmail.com>
2023-11-24 16:19:54 -0300

    Add aula 23/11 Geografia GCM VG

--- PrincipalGcmVgPre2023.php
+++ PrincipalGcmVgPre2023.php
@@ -391,7 +391,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             </p>
                             <div class="collapse" id="collapseGeoMarcelo">
                                 <div class="card card-body">
-                                    <!-- <a href="GUARDAVG2023_HISTORIA_BIDU_AULA01.php">AULA 01</a> -->
+                                    <a href="GUARDAVG2023_GEOGRAFIA_MARCELO_AULA01.php">AULA 01</a>
                                 </div>
                             </div>
                         </div>
