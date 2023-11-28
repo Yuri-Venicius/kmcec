8de9c7c664ad3786b3a256cd95fcd41c011e0bf7
Yuri Oliveira <yvenicios@gmail.com>
2023-11-28 12:46:23 -0300

    Add aula 27/11 GCM VG - PT HELIO

--- PrincipalGcmVgPre2023.php
+++ PrincipalGcmVgPre2023.php
@@ -225,6 +225,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="card card-body">
                                     <a href="GUARDAVG2023_PT_HELIO_AULA01.php">AULA 01</a>
                                     <a href="GUARDAVG2023_PT_HELIO_AULA02.php">AULA 02</a>
+                                    <a href="GUARDAVG2023_PT_HELIO_AULA03.php">AULA 03</a>
                                     <p></p>
                                 </div>
                             </div>
