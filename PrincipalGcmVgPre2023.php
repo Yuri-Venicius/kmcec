14709ff0404b55269ebd5ada309959c1cc70ed6c
Yuri Oliveira <yvenicios@gmail.com>
2023-12-02 17:39:07 -0300

    Add aula 01/12 Cleide Adm -- GCMVG

--- PrincipalGcmVgPre2023.php
+++ PrincipalGcmVgPre2023.php
@@ -294,6 +294,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <a href="GUARDAVG2023_DIR_ADM_CLEIDE_AULA01.php">AULA 01</a>
                                     <a href="GUARDAVG2023_DIR_ADM_CLEIDE_AULA02.php">AULA 02</a>
                                     <a href="GUARDAVG2023_DIR_ADM_CLEIDE_AULA03.php">AULA 03</a>
+                                    <a href="GUARDAVG2023_DIR_ADM_CLEIDE_AULA04.php">AULA 04</a>
                                 </div>
                             </div>
                         </div>
