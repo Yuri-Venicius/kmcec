a05869461737447b58d41f6ab796f659dd7a2d66
Yuri Oliveira <yvenicios@gmail.com>
2023-12-02 17:40:22 -0300

    Add aula 02/12 Leg Ebserh -- Davoli - Ebserh todos

--- PrincipalEbserhBasico.php
+++ PrincipalEbserhBasico.php
@@ -201,6 +201,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <a href="EBSERH_LEG_DAVOLI_AULA07.php">AULA 07</a>
                                     <a href="EBSERH_LEG_DAVOLI_AULA08.php">AULA 08</a>
                                     <a href="EBSERH_LEG_DAVOLI_AULA09.php">AULA 09</a>
+                                    <a href="EBSERH_LEG_DAVOLI_AULA10.php">AULA 10</a>
                                 </div>
                             </div>
                         </div>
