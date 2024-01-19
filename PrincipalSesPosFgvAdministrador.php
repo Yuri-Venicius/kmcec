278652d891cbea3870bf162987b517d30bb7298c
Yuri Oliveira <yvenicios@gmail.com>
2024-01-19 11:18:53 -0300

    Add aula 18-01 RLM IGOR SES TODOS

--- PrincipalSesPosFgvAdministrador.php
+++ PrincipalSesPosFgvAdministrador.php
@@ -213,6 +213,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapseRlmIgor">
                                     <div class="card card-body">
                                         <a href="SES_POS_TODOS_RLM_IGOR_AULA01.php">AULA 01</a>
+                                        <a href="SES_POS_TODOS_RLM_IGOR_AULA02.php">AULA 02</a>
                                     </div>
                                 </div>
                             </div>
