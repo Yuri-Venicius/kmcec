0181b1576d5da39e13fac0cc48609e51474ab5ca
Yuri Oliveira <yvenicios@gmail.com>
2024-01-24 12:08:08 -0300

    Add aula 23/01 Portugues Helio - SES TODOS

--- PrincipalSesPosFgvBasicoMedio.php
+++ PrincipalSesPosFgvBasicoMedio.php
@@ -171,6 +171,8 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                     <div class="card card-body">
                                         <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA01.php">AULA 01</a>
                                         <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA02.php">AULA 02</a>
+                                        <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA03.php">AULA 03</a>
+
                                     </div>
                                 </div>
                             </div>
--- SES_POS_TODOS_PORTUGUES_HELIO_AULA03.php
+++ SES_POS_TODOS_PORTUGUES_HELIO_AULA03.php
@@ -86,12 +86,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
         <!-- Page Content  -->
         <div id="content">
             <h2>SES-MT PÓS EDITAL - PORTUGUÊS | PROFESSOR HÉLIO TAQUES</h2>
-            <h3>AULA 02</h3>
+            <h3>AULA 03</h3>
             <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_PORTUGUES_HELIO_AULA01.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_PORTUGUES_HELIO_AULA03.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_PORTUGUES_HELIO_AULA02.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_PORTUGUES_HELIO_AULA04.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/l8Q6cBZ2Tws" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/blYI3cA3XMg" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
             </div>
             <p></p>
         </div>
