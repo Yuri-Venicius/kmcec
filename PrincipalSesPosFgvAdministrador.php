fcd7253fae25f099631cea94312042fddbee50dc
Yuri Oliveira <yvenicios@gmail.com>
2024-01-26 12:34:33 -0300

    Add Portugues Helio aula 04 - 25/01 Ses Todos

--- PrincipalSesPosFgvAdministrador.php
+++ PrincipalSesPosFgvAdministrador.php
@@ -172,6 +172,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA01.php">AULA 01</a>
                                         <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA02.php">AULA 02</a>
                                         <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA03.php">AULA 03</a>
+                                        <a href="SES_POS_TODOS_PORTUGUES_HELIO_AULA04.php">AULA 04</a>
                                     </div>
                                 </div>
                             </div>
