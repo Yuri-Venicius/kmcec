089c1bd5212c76bbf0c37d19ffafc4fdf6662899
Yuri Oliveira <yvenicios@gmail.com>
2024-02-01 18:56:19 -0300

    ADD Pasta especifico fisioterapia maritza

--- PrincipalSesPosFgvFisioterapia.php
+++ PrincipalSesPosFgvFisioterapia.php
@@ -210,6 +210,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
+                                <h5 class="card-title">ESPECÍFICO FISIOTERAPIA - MARITZA</h5>
+                                <p class="card-text">
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseEspFisioMaritza" aria-expanded="false" aria-controls="collapseExample">
+                                        <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                    </button>
+                                    <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1ppm5GfbDnHLMKLasHJDxfchENvAgyX4G" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                                </p>
+                                <div class="collapse" id="collapseEspFisioMaritza">
+                                    <div class="card card-body">
+                                        <a>Ainda não há conteúdo gravado</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p></p>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
                                 <h5 class="card-title">PORTUGUÊS - HÉLIO TAQUES</h5>
                                 <p class="card-text">
                                     <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseConstNeto" aria-expanded="false" aria-controls="collapseExample">
