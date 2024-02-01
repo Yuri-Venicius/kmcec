b4090711f4b1461c8d81172a357e1c5dc764b6ea
Yuri Oliveira <yvenicios@gmail.com>
2024-02-01 11:47:57 -0300

    Add aula 06 Leg Sus - Davoli SES TODOS

--- PrincipalSesPosFgvTecLetrasPedago.php
+++ PrincipalSesPosFgvTecLetrasPedago.php
@@ -243,6 +243,8 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA03.php">AULA 03</a>
                                         <a href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA04.php">AULA 04</a>
                                         <a href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA05.php">AULA 05</a>
+                                        <a href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA06.php">AULA 06</a>
+
                                     </div>
                                 </div>
                             </div>
--- SES_POS_TODOS_LEG_SUS_DAVOLI_AULA06.php
+++ SES_POS_TODOS_LEG_SUS_DAVOLI_AULA06.php
@@ -86,12 +86,14 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
         <!-- Page Content  -->
         <div id="content">
             <h2>SES-MT PÓS EDITAL - LEGISLAÇÃO DO SUS | PROFESSOR FERNANDO DAVOLI</h2>
-            <h3>AULA 05</h3>
+            <h3>AULA 06</h3>
             <div>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA04.php">Anterior</a>
-                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA06.php">Próximo</a>
+                <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA05.php">Anterior</a>
+                <!-- <a style="align-items: flex-end;" class="btn btn-primary" href="SES_POS_TODOS_LEG_SUS_DAVOLI_AULA07.php">Próximo</a> -->
                 <p></p>
-                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://www.youtube.com/embed/K8vu46IW6dI" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                <div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;">
+                    <iframe src="https://www.youtube.com/embed/v_2ahuYP_j0" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe>
+                </div>
             </div>
             <p></p>
         </div>
