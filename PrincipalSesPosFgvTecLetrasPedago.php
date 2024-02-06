6c7462bd13c347e2eb4bb9354ad53ad1cb2823f9
Yuri Oliveira <yvenicios@gmail.com>
2024-02-06 10:10:52 -0300

    Add aula 05/02 - Portugues Gouveia - SES TODOS

--- PrincipalSesPosFgvTecLetrasPedago.php
+++ PrincipalSesPosFgvTecLetrasPedago.php
@@ -243,6 +243,8 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                 <div class="collapse" id="collapsePtGouveia">
                                     <div class="card card-body">
                                         <a href="SES_POS_TODOS_PORTUGUES_GOUVEIA_AULA01.php">AULA 01</a>
+                                        <a href="SES_POS_TODOS_PORTUGUES_GOUVEIA_AULA02.php">AULA 02</a>
+
                                     </div>
                                 </div>
                             </div>
