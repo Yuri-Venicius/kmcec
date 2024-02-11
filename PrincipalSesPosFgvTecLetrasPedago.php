c71f089c4b5fc356abd03b9b1da04522fc5ed597
Yuri Oliveira <yvenicios@gmail.com>
2024-02-10 23:08:26 -0300

    Add aula 10-02 Esp pedagogia Hleio Taques

--- PrincipalSesPosFgvTecLetrasPedago.php
+++ PrincipalSesPosFgvTecLetrasPedago.php
@@ -209,6 +209,26 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
+                                <h5 class="card-title">ESPECÍFICO LETRAS/PEDAGOGIA - HÉLIO TAQUES</h5>
+                                <p class="card-text">
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseEspLetrasHelio" aria-expanded="false" aria-controls="collapseExample">
+                                        <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                    </button>
+                                    <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1oe-pBezZWweoPV0xL63yOtaBqdsoC5uz" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                                </p>
+                                <div class="collapse" id="collapseEspLetrasHelio">
+                                    <div class="card card-body">
+                                        <a href="SES_POS_ESP_PEDAGOGO_HELIO_AULA01.php">AULA 01</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p></p>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
                                 <h5 class="card-title">PORTUGUÊS - HÉLIO TAQUES</h5>
                                 <p class="card-text">
                                     <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseConstNeto" aria-expanded="false" aria-controls="collapseExample">
