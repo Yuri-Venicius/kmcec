bdbadcbd413abba5a0b2d9287ff276618a55e1ed
Yuri Oliveira <yvenicios@gmail.com>
2024-02-29 19:38:46 -0400

    Add aula e link para pasta de portugues helio - seduc pre 24

--- PrincipalSeducPre2024.php
+++ PrincipalSeducPre2024.php
@@ -165,6 +165,27 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="collapse" id="collapseLdbOzanil">
                                 <div class="card card-body">
                                     <a href="SEDUC_PRE_2024_LDB_OZANIL_AULA01.php">AULA 01</a>
+                                    <a href="SEDUC_PRE_2024_LDB_OZANIL_AULA02.php">AULA 02</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p></p>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">PORTUGUÊS - HÉLIO TAQUES</h5>
+                            <p class="card-text">
+                                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapsePtHelio" aria-expanded="false" aria-controls="collapseExample">
+                                    <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
+                                </button>
+                                <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/15sMuAzJHZPbIm5MFSVJSWq_kZ3sEva1t" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
+                            </p>
+                            <div class="collapse" id="collapsePtHelio">
+                                <div class="card card-body">
+                                    <a>Ainda não há conteúdo gravado</a>
                                 </div>
                             </div>
                         </div>
