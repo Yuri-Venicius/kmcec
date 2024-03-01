b4876d0f21c7db1bde59c3edd6bf5a3e5be2111c
Yuri Oliveira <yvenicios@gmail.com>
2024-03-01 17:51:19 -0400

    Add Novo Curso Caixa Economica 2024 Online

--- index.php
+++ index.php
@@ -127,6 +127,28 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                 <?php endif; ?>
 
                 <?php
+                $codCurso = 3614;
+                $userTemp = verificaAcessoAoCurso($conexao, $usuarioLogado['id'], $codCurso);
+                if (
+                    $verificaAdm['nivelAcesso'] == '1' || ($userTemp['idUsuario'] == $usuarioLogado['id'] && $userTemp['idCurso'] == $codCurso && $userTemp['statusAcesso'] == 'ATIVO')
+                ) : ?>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Caixa Econômica Federal Pós edital [Online]</h5>
+                                <p class="card-text">Curso preparatório pós edital online ao vivo para o concurso da Caixa Economica Federal 2024.
+                                </p>
+                                </p>
+                                <p></p>
+                                <a href="PrincipalCaixaEconomica2024.php" class="btn btn-primary">Acessar</a>
+                                <!-- <a style="margin-left: 30px;" href="" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a> -->
+                            </div>
+                        </div>
+                        <p></p>
+                    </div>
+                <?php endif; ?>
+                
+                <?php
                 $codCurso = 3606;
                 $userTemp = verificaAcessoAoCurso($conexao, $usuarioLogado['id'], $codCurso);
                 if (
