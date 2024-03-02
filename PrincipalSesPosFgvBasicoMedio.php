e9c4c599f11bb75f65b31a379d4b2ba9a89226f4
Yuri Oliveira <yvenicios@gmail.com>
2024-03-02 01:24:49 -0400

    Add aula 01/03 - RLM Igor Brasil - SES TODOS

--- PrincipalSesPosFgvBasicoMedio.php
+++ PrincipalSesPosFgvBasicoMedio.php
@@ -270,6 +270,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                                         <a href="SES_POS_TODOS_RLM_IGOR_AULA02.php">AULA 02</a>
                                         <a href="SES_POS_TODOS_RLM_IGOR_AULA03.php">AULA 03</a>
                                         <a href="SES_POS_TODOS_RLM_IGOR_AULA04.php">AULA 04</a>
+                                        <a href="SES_POS_TODOS_RLM_IGOR_AULA05.php">AULA 05</a>
                                     </div>
                                 </div>
                             </div>
