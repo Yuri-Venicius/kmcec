d5a2c555a65440e82cef1ad1305270d2cc7586d9
Yuri Oliveira <yvenicios@gmail.com>
2024-03-04 17:04:18 -0400

    Fix: id Collapse historia bidu os

--- PrincipalSesPosFgvTecEnf.php
+++ PrincipalSesPosFgvTecEnf.php
@@ -439,12 +439,12 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             <div class="card-body">
                                 <h5 class="card-title">HISTÓRIA REGIONAL - CARLOS BIDU</h5>
                                 <p class="card-text">
-                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseHistBidu" aria-expanded="false" aria-controls="collapseExample">
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseHistBiduPos" aria-expanded="false" aria-controls="collapseExample">
                                         <i class="fas fa-play-circle"></i> AULAS DISPONÍVEIS
                                     </button>
                                     <a style="margin-left: 30px;" href="https://drive.google.com/drive/folders/1rpfBhYT0zDUqQK_ZUiFO_9_-N5x3l5KK?usp=drive_link" class="btn btn-primary"><i class="fas fa-book-open"></i> Materiais</a>
                                 </p>
-                                <div class="collapse" id="collapseHistBidu">
+                                <div class="collapse" id="collapseHistBiduPos">
                                     <div class="card card-body">
                                         <a href="SES_POS_TODOS_HISTORIA_BIDU_AULA01.php">AULA 01</a>
                                     </div>
