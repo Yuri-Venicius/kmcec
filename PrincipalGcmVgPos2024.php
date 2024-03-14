227b46117ee8adc548179a9d7075b572ba2781cd
Yuri Oliveira <yvenicios@gmail.com>
2024-03-13 23:48:01 -0300

    Add aul Informatica - GCM VG

--- PrincipalGcmVgPos2024.php
+++ PrincipalGcmVgPos2024.php
@@ -188,7 +188,7 @@ $verificaAdm = buscaDadosBasicosUsuario($conexao, $_SESSION['usuario']);
                             </p>
                             <div class="collapse" id="collapsInformaticaJoao">
                                 <div class="card card-body">
-                                    <a>Ainda não há gravação de conteúdo transmitido.</a>
+                                    <a href="GUARDAVG_POS_2024_JOAO_INFO_AULA01.php">AULA 01</a>
                                 </div>
                             </div>
                         </div>
